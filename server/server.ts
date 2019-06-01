4f17e9932e967aeb9ba7332642d47052cec447c7
Slawomir Kussowski <skussowski@advaoptical.com>
2019-06-01 21:15:01 +0200

    Packages update

--- server/server.ts
+++ server/server.ts
@@ -12,5 +12,5 @@ app.route('/api/courses/:id').get(getCourseById);
 app.route('/api/lessons').get(searchLessons);
 
 const httpServer = app.listen(9000, () => {
-	console.log('HTTP REST API Server running at http://localhost:' + httpServer.address().port);
+  console.log('HTTP REST API Server running at http://localhost:' + httpServer.address().port);
 });
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Rachunek } from '../model/rachunek';
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class FirebaseService {
-	constructor(public db: AngularFirestore) {}
+  constructor(public db: AngularFirestore) {}
 
-	odczytajRachunki() {
-		return this.db.collection('rachunki').valueChanges();
-	}
+  odczytajRachunki() {
+    return this.db.collection('rachunki').valueChanges();
+  }
 }
