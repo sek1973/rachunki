c126ef2af50d50122769951c959d856930dc2bc2
Slawomir Kussowski <skussowski@advaoptical.com>
2019-06-02 09:36:22 +0200

    Minor changes

--- src/app/services/rachunki.datasource.ts
+++ src/app/services/rachunki.datasource.ts
@@ -7,28 +7,28 @@ import { Rachunek } from '../model/rachunek';
 import { FirebaseService } from './firebase.service';
 
 export class LessonsDataSource implements DataSource<Rachunek> {
-	private rachunkiSubject = new BehaviorSubject<Rachunek[]>([]);
-	private loadingSubject = new BehaviorSubject<boolean>(false);
+  private rachunkiSubject = new BehaviorSubject<Rachunek[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
 
-	public loading$ = this.loadingSubject.asObservable();
+  public loading$ = this.loadingSubject.asObservable();
 
-	constructor(private firebaseService: FirebaseService) {}
+  constructor(private firebaseService: FirebaseService) {}
 
-	connect(collectionViewer: CollectionViewer): Observable<Rachunek[]> {
-		return this.rachunkiSubject.asObservable();
-	}
+  connect(collectionViewer: CollectionViewer): Observable<Rachunek[]> {
+    return this.rachunkiSubject.asObservable();
+  }
 
-	disconnect(collectionViewer: CollectionViewer): void {
-		this.rachunkiSubject.complete();
-		this.loadingSubject.complete();
-	}
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.rachunkiSubject.complete();
+    this.loadingSubject.complete();
+  }
 
-	wczytajRachunki(courseId: number, filter = '', sortDirection = 'asc', pageIndex = 0, pageSize = 3) {
-		this.loadingSubject.next(true);
+  wczytajRachunki(courseId: number, filter = '', sortDirection = 'asc', pageIndex = 0, pageSize = 3) {
+    this.loadingSubject.next(true);
 
-		this.firebaseService
-			.odczytajRachunki()
-			.pipe(catchError(() => of([])), finalize(() => this.loadingSubject.next(false)))
-			.subscribe((lessons) => this.rachunkiSubject.next(lessons));
-	}
+    this.firebaseService
+      .odczytajRachunki()
+      .pipe(catchError(() => of([])), finalize(() => this.loadingSubject.next(false)))
+      .subscribe((lessons) => this.rachunkiSubject.next(lessons));
+  }
 }
