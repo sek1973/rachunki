2091d505b27a06b1cd1694e98fc000b8f25f82e4
Slawomir Kussowski <skussowski@advaoptical.com>
2019-10-26 16:52:30 +0200

    Service - added new methods for fetching data

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 
 import { Bill } from '../model/bill';
 import { Payment } from '../model/payment';
+import { mergeMap, map } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root',
@@ -12,7 +13,8 @@ export class FirebaseService {
 	constructor(public db: AngularFirestore) { }
 
 	fetchBills(): Observable<Bill[]> {
-		return this.db.collection<Bill>('bills').valueChanges();
+		return this.db.collection<Bill>('bills')
+			.valueChanges({ idField: 'uid' });
 	}
 
 	fetchPayments(id: number): Observable<Payment[]> {
@@ -21,5 +23,24 @@ export class FirebaseService {
 			return query.valueChanges();
 		}
 		return of([]);
+	}
+
+	fetchPaymentsForBill(bill: Bill): Observable<Bill> {
+		if (bill) {
+			return this.fetchPayments(bill.id).pipe(map(payments => {
+				bill.payments = payments;
+				return bill;
+			}));
+		}
+		return of(null);
+	}
+
+	fetchBill(id: number): Observable<Bill> {
+		if (id !== undefined) {
+			const query = this.db.doc<Bill>('bills/' + id);
+			return query.valueChanges()
+				.pipe(mergeMap(bill => this.fetchPaymentsForBill(bill)));
+		}
+		return of(null);
 	}
 }
