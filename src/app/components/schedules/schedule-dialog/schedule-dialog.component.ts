b34a1836c2c9814c3c6bf1865ac90a6d159b1745
Slawomir Kussowski <skussowski@advaoptical.com>
2019-11-16 11:14:20 +0100

    Schedules dialog component modification

--- src/app/components/schedules/schedule-dialog/schedule-dialog.component.ts
+++ src/app/components/schedules/schedule-dialog/schedule-dialog.component.ts
@@ -1,6 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { of } from 'rxjs';
+import { delay, mergeMap } from 'rxjs/operators';
+import { getSafe } from 'src/app/helpers';
+import { Schedule } from 'src/app/model/schedule';
 
+export interface ScheduleDialogData {
+  schedule?: Schedule
+}
 @Component({
   selector: 'app-schedule-dialog',
   templateUrl: './schedule-dialog.component.html',
@@ -8,14 +16,51 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 })
 export class ScheduleDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-    public dialogRef: MatDialogRef<ScheduleDialogComponent>) { }
+  schedule: Schedule;
+  dialogTitle: string;
+  dialogMode: 'add' | 'edit' = 'add';
+  loading = true;
+
+  form: FormGroup = new FormGroup({
+    uid: new FormControl,
+    deadline: new FormControl(),
+    sum: new FormControl(),
+    description: new FormControl()
+  });
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ScheduleDialogData,
+    public dialogRef: MatDialogRef<ScheduleDialogComponent>) {
+    this.schedule = getSafe(() => data.schedule);
+    this.dialogTitle = (this.schedule ? 'Edytuj' : 'Dodaj') + ' planowaną płatność';
+    this.dialogMode = this.schedule ? 'edit' : 'add';
+    this.loading = false;
+  }
 
   ngOnInit() {
   }
 
   closeDialog() {
     this.dialogRef.close('cancel');
+  }
+
+  saveData() {
+    this.loading = true;
+    const request = of({})
+      .pipe(
+        delay(1000),
+        mergeMap(() => this.schedule ? of(true) : of(false)),
+        delay(1000));
+    request.subscribe(resp => {
+      this.dialogRef.close('saved');
+    },
+      error => {
+        console.error(error);
+        this.loading = false;
+      });
+  }
+
+  cloneData() {
+
   }
 
 }
--- src/app/components/schedules/schedules.component.ts
+++ src/app/components/schedules/schedules.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { TableComponent } from './../tools/table/table.component';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { Schedule } from 'src/app/model/schedule';
 
@@ -20,6 +21,7 @@ export class SchedulesComponent implements OnInit {
   get billUid(): string {
     return this._builUid;
   }
+  @ViewChild('table', { static: false, read: TableComponent }) table: TableComponent;
 
   activeRow: any;
 
@@ -54,21 +56,11 @@ export class SchedulesComponent implements OnInit {
     this.dataSource.load();
   }
 
-  addDialog(): void {
+  openDialog(): void {
+    const schedule: Schedule = this.table.activeRow;
     const dialogRef = this.dialog.open(ScheduleDialogComponent, {
-      width: '250px',
-      data: { mode: 'add' }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
-
-  editDialog(): void {
-    const dialogRef = this.dialog.open(ScheduleDialogComponent, {
-      width: '250px',
-      data: { mode: 'edit' }
+      width: '500px',
+      data: { schedule: schedule }
     });
 
     dialogRef.afterClosed().subscribe(result => {
